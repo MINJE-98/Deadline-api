b184cc01ac55f633b6dfd2e6707ceaf39369d135
MINJE-98 <jmj012100@gmail.com>
2021-05-30 18:59:40 +0900

    nest init

---
+++ old/service/DB.config.js
@@ -0,0 +1,8 @@
+module.exports = ({
+    host: 'deadline.cuqdvpvwbmig.ap-northeast-2.rds.amazonaws.com',
+    user: 'deadline',
+    password: 'jmj0whalswpcom!',
+    database: 'deadline',
+    connectTimeout: 2000,
+    connectionLimit: 30,
+}) 
---
+++ old/service/facebook.config.js
@@ -0,0 +1,5 @@
+module.exports =({
+    appID: '2604738803080027',
+    clientSecret: 'f675b2dc9c6186f3b353b6550ee029fe',
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+}) 
