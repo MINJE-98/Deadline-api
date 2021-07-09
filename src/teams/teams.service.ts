aab1b828a2d90b2d03433fa03eda97d8188b546a
MINJE-98 <jmj012100@gmail.com>
2021-07-09 21:10:34 +0900

    close #18 add Teams swagger, endpoint

--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Teams } from 'src/entities';
 
 @Injectable()
-export class TeamsService {}
+export class TeamsService {
+  createTeam(createTeamDto: Teams) {
+    return 'createTeam';
+  }
+
+  findTeam(id: number) {
+    return `findTeam`;
+  }
+
+  updateTeam(id: number, updateTeamDto: Teams) {
+    return `updateTeam`;
+  }
+
+  removeTeam(id: number) {
+    return `removeTeam`;
+  }
+
+  createTeamMembers(email: string) {
+    return `createTeamMembers ${email}`;
+  }
+
+  getTeamMembers(param: string) {
+    return `getTeamMembers ${param}`;
+  }
+  updateTeamMembersState(email: string) {
+    return `updateTeamMembersState ${email}`;
+  }
+  deleteTeamMembersState(email: string) {
+    return `deleteTeamMembersState ${email}`;
+  }
+}
