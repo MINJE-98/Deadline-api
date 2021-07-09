281490acfa7220db41736f3db326c60b8826071e
MINJE-98 <jmj012100@gmail.com>
2021-07-09 21:09:48 +0900

    close #17 add Users swagger, endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,63 @@
-import { Controller, Get, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiDefaultResponse,
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TokenCheck } from 'src/auth/token.check.guard';
+import { Users } from '../entities/Users';
+import { UsersService } from './users.service';
 
+// @UseGuards(TokenCheck)
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
-  @Get()
-  getUsers() {}
+  constructor(private readonly usersService: UsersService) {}
+
+  /**
+   * TODO
+   *   CRUD
+   *
+   */
   @Post()
-  postUsers() {}
-  @Patch()
-  updateUsers() {
-    ('');
+  @ApiOperation({ summary: '유저 생성', description: '유저를 생성합니다.' })
+  @ApiResponse({ status: 200, description: '성공', type: Users })
+  @ApiResponse({ status: 500, description: '서버에러' })
+  createUser(@Body() data: Users) {
+    return this.usersService.createUser(data);
+  }
+
+  @Get(':userid')
+  @ApiOperation({ summary: '유저 조회', description: '유저를 조회합니다.' })
+  @ApiResponse({ status: 200, description: '성공', type: Users })
+  findUser(@Param('userid') userid: string) {
+    return this.usersService.findUser(+userid);
+  }
+
+  @Patch(':userid')
+  @ApiOperation({ summary: '유저 수정', description: '유저를 수정합니다' })
+  @ApiResponse({ status: 200, description: '성공', type: Users })
+  updateUser(@Param('userid') userid: string) {
+    return this.usersService.updateUser(+userid);
+  }
+
+  @Delete(':userid')
+  @ApiOperation({ summary: '유저 삭제', description: '유저를 삭제합니다.' })
+  @ApiResponse({ status: 200, description: '성공', type: Users })
+  removeUser(@Param('userid') userid: string) {
+    return this.usersService.removeUser(+userid);
   }
 }
