6c5064e07925d15d8d108c7bdf51b1a25efece8f
MINJE-98 <jmj012100@gmail.com>
2021-07-13 20:42:33 +0900

    edit 수정 기능 비활성화, CRD 기능 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,31 +2,29 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   Patch,
   Param,
   Delete,
   UseGuards,
   Headers,
-  Request,
+  Query,
+  Body,
 } from '@nestjs/common';
-import {
-  ApiBody,
-  ApiCreatedResponse,
-  ApiDefaultResponse,
-  ApiOperation,
-  ApiProperty,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
-import { request } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TokenCheck } from 'src/auth/token.check.guard';
-import { Token } from '../common/dto';
+import { User } from 'src/common/decorator/user.decorator';
+import { Token } from 'src/common/dto';
+import { AccessToken, SocialId } from '../common/dto';
 import { Users } from '../entities/Users';
 import { UsersService } from './users.service';
 
 @UseGuards(TokenCheck)
 @ApiTags('Users')
+@ApiResponse({
+  status: 404,
+  description: '유효한 토큰이 아닙니다.',
+  type: Token,
+})
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -38,31 +36,39 @@ export class UsersController {
    */
   @Post()
   @ApiOperation({ summary: '유저 생성', description: '유저를 생성합니다.' })
-  @ApiResponse({ status: 200, description: '성공', type: Users })
-  @ApiResponse({ status: 500, description: '서버에러' })
-  createUser(@Headers() query: Token, @Request() res: any) {
-    // console.log(user);
-    return this.usersService.createUser(res);
+  @ApiResponse({ status: 201, description: '유저 생성 성공', type: Users })
+  createUser(@Headers() AccessToken: AccessToken, @User() userinfo: Users) {
+    return this.usersService.createUser(userinfo);
   }
 
-  @Get(':userid')
+  @Get(':socialId')
   @ApiOperation({ summary: '유저 조회', description: '유저를 조회합니다.' })
   @ApiResponse({ status: 200, description: '성공', type: Users })
-  findUser(@Param('userid') userid: string) {
-    return this.usersService.findUser(+userid);
+  findUser(
+    @Headers() AccessToken: AccessToken,
+    @Param('socialId') socialId: number,
+  ) {
+    return this.usersService.findUser(socialId);
   }
 
-  @Patch(':userid')
-  @ApiOperation({ summary: '유저 수정', description: '유저를 수정합니다' })
-  @ApiResponse({ status: 200, description: '성공', type: Users })
-  updateUser(@Param('userid') userid: string) {
-    return this.usersService.updateUser(+userid);
-  }
+  // @Patch(':socialId')
+  // @ApiOperation({ summary: '유저 수정', description: '유저를 수정합니다' })
+  // @ApiResponse({ status: 200, description: '성공', type: Users })
+  // updateUser(
+  //   @Headers() token: Token,
+  //   @Param('socialId') socialId: number,
+  //   @Body() body: Users,
+  // ) {
+  //   return this.usersService.updateUser(socialId);
+  // }
 
-  @Delete(':userid')
+  @Delete(':socialId')
   @ApiOperation({ summary: '유저 삭제', description: '유저를 삭제합니다.' })
   @ApiResponse({ status: 200, description: '성공', type: Users })
-  removeUser(@Param('userid') userid: string) {
-    return this.usersService.removeUser(+userid);
+  removeUser(
+    @Headers() AccessToken: AccessToken,
+    @Param('socialId') socialId: number,
+  ) {
+    return this.usersService.removeUser(socialId);
   }
 }
