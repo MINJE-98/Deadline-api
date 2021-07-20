b304f177dbbf98d6e17f65aa338bfbe67cd6c48a
MINJE-98 <jmj012100@gmail.com>
2021-07-20 17:23:23 +0900

    edit: 팀관련 dto 이름 변경으로 수정 및 tag dto 추가

--- src/common/dto/index.ts
+++ src/common/dto/index.ts
@@ -1,4 +1,7 @@
+export { TagUpdate } from './tags/tagUpdate.dto';
+
+export { TeamMembersUpdate } from './teams/teamMembersUpdate.dto';
 export { TeamId } from './teams/teamId.dto';
-export { TeamName } from './teams/teamName.dto';
+export { TeamsUpdate } from './teams/teamsUpdate.dto';
 export { TokenAuthError } from './httpException/tokenAuthError.dto';
 export { AccessToken } from './token/accessToken.dto';
