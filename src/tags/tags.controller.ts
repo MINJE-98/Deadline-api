5aef9aa872cdd01fc8bcddb42ae8ca76fa71c26a
MINJE-98 <jmj012100@gmail.com>
2021-07-20 17:23:56 +0900

    close #27 tags CURD 추가

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -6,56 +6,108 @@ import {
   Patch,
   Param,
   Delete,
+  Headers,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiHeader,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TokenCheck } from 'src/auth/token.check.guard';
+import { UserInfo } from 'src/common/decorator';
+import { TagUpdate, TokenAuthError } from 'src/common/dto';
 import { Tags } from 'src/entities';
 import { TagsService } from './tags.service';
+
+@UseGuards(TokenCheck)
 @ApiTags('Tags')
-@Controller('tags')
+@ApiHeader({
+  name: 'accesstoken',
+  required: true,
+  description: '토큰',
+})
+@ApiResponse({
+  status: 404,
+  description: '유효한 토큰이 아닙니다.',
+  type: TokenAuthError,
+})
+@Controller('teams')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
-  @Post()
+  @Post('/:teamid/tags/:tagname')
   @ApiOperation({ summary: '테그 생성', description: '테그을 생성합니다.' })
   @ApiResponse({ status: 200, description: '성공', type: Tags })
-  createTag(@Body() createTagDto: Tags) {
-    return this.tagsService.createTag(createTagDto);
+  createTags(
+    @Headers('accessToken') AccessToken,
+    @UserInfo() userinfo,
+    @Param('teamid') teamid: number,
+    @Param('tagname') tagname: string,
+  ) {
+    return this.tagsService.createTags(userinfo, teamid, tagname);
   }
 
-  @Get()
-  @ApiOperation({ summary: '테그 조회', description: '테그를 조회합니다.' })
-  @ApiResponse({ status: 200, description: '성공', type: Tags })
-  findAllTags() {
-    return this.tagsService.findAllTags();
-  }
-
-  @Get(':tagId')
+  @Get('/:teamid/tags')
   @ApiOperation({
-    summary: '특정 테그 조회',
-    description: '특정 테그을 조회합니다.',
+    summary: '테그를 조회',
+    description: '테그를 조회합니다.',
+  })
+  @ApiQuery({
+    required: false,
+    name: 'tagid',
+  })
+  @ApiQuery({
+    required: false,
+    name: 'tagname',
   })
   @ApiResponse({ status: 200, description: '성공', type: Tags })
-  findTag(@Param('tagId') tagId: string) {
-    return this.tagsService.findTag(+tagId);
+  findTags(
+    @Headers('accessToken') AccessToken,
+    @UserInfo() userinfo,
+    @Param('teamid') teamid: number,
+    @Query('tagid') tagid: number,
+    @Query('tagname') tagname: string,
+  ) {
+    return this.tagsService.findTags(userinfo, teamid, tagid, tagname);
   }
 
-  @Patch(':tagId')
+  @Patch('/:teamid/tags/:tagid')
   @ApiOperation({
-    summary: '특정 테그 수정',
-    description: '특정 테그를 수정합니다.',
+    summary: '테그 수정',
+    description: '테그를 수정합니다.',
+  })
+  @ApiBody({
+    type: TagUpdate,
+    description: '변경할 테그명',
   })
   @ApiResponse({ status: 200, description: '성공', type: Tags })
-  updateTag(@Param('tagId') tagId: string, @Body() updateTagDto: Tags) {
-    return this.tagsService.updateTag(+tagId, updateTagDto);
+  updateTag(
+    @Headers('accessToken') AccessToken,
+    @UserInfo() userinfo,
+    @Param('teamid') teamid: number,
+    @Param('tagid') tagid: number,
+    @Body('name') tagname,
+  ) {
+    return this.tagsService.updateTag(userinfo, teamid, tagid, tagname);
   }
 
-  @Delete(':tagId')
+  @Delete('/:teamid/tags/:tagid')
   @ApiOperation({
-    summary: '특정 테그 삭제',
-    description: '특정 테그를 삭제합니다.',
+    summary: '테그 삭제',
+    description: '테그를 삭제합니다.',
   })
   @ApiResponse({ status: 200, description: '성공', type: Tags })
-  removeTag(@Param('tagId') id: string) {
-    return this.tagsService.removeTag(+id);
+  removeTags(
+    @Headers('accessToken') AccessToken,
+    @UserInfo() userinfo,
+    @Param('teamid') teamid: number,
+    @Param('tagid') tagid: number,
+  ) {
+    return this.tagsService.removeTags(userinfo, teamid, tagid);
   }
 }
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,25 +1,182 @@
-import { Injectable } from '@nestjs/common';
-import { Tags } from 'src/entities';
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tags, TeamMembers, Teams, Users } from 'src/entities';
+import { Like, Repository } from 'typeorm';
 @Injectable()
 export class TagsService {
-  createTag(createTagDto: Tags) {
-    return 'createTag';
+  constructor(
+    @InjectRepository(Teams) private teamsRepository: Repository<Teams>,
+    @InjectRepository(TeamMembers)
+    private teamMembersRepository: Repository<TeamMembers>,
+    @InjectRepository(Users) private usersRepository: Repository<Users>,
+    @InjectRepository(Tags) private tagsRepository: Repository<Tags>,
+  ) {}
+  async createTags(userinfo: Users, teamid: number, tagname: string) {
+    // 팀에 가입되어있는지?
+    const isjoined = await this.teamMembersRepository.findOne({
+      userid: userinfo.id,
+      teamid: teamid,
+    });
+    // no
+    if (isjoined == undefined) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '팀에 가입되어 있지 않습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    // yes but.
+    // 테그를 추가할 권한이 있는지?
+    if (isjoined.state) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '권한이 없습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const tags = new Tags();
+    const now = new Date();
+    tags.name = tagname;
+    tags.teamid = teamid;
+    tags.createdAt = now;
+    tags.updatedAt = now;
+    return await this.tagsRepository.save(tags);
   }
 
-  findAllTags() {
-    return `findAllTags`;
+  async findTags(
+    userinfo: Users,
+    teamid: number,
+    tagid: number,
+    tagname: string,
+  ) {
+    // 팀에 가입되어있는지?
+    const isjoined = await this.teamMembersRepository.findOne({
+      userid: userinfo.id,
+      teamid: teamid,
+    });
+    // no
+    if (isjoined == undefined) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '팀에 가입되어 있지 않습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    // 테그 아이디로 검색
+    if (!!tagid)
+      return await this.tagsRepository.findOne({
+        id: tagid,
+      });
+    // 테그 명으로 검색
+    if (!!tagname)
+      return await this.tagsRepository.findOne({
+        name: Like(`%${tagname}%`),
+      });
+
+    // 옵션이 없을때 전체 검색
+    return await this.tagsRepository.find({
+      teamid: teamid,
+    });
   }
 
-  findTag(id: number) {
-    return `findTag`;
+  async updateTag(
+    userinfo: Users,
+    teamid: number,
+    tagid: number,
+    tagname: string,
+  ) {
+    const isjoined = await this.teamMembersRepository.findOne({
+      userid: userinfo.id,
+      teamid: teamid,
+    });
+    // no
+    if (isjoined == undefined) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '팀에 가입되어 있지 않습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    // 수정할 권한이 있는가?
+    if (isjoined.state) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '권한이 없습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    // 해당 테그가 있는가?
+    const tags = await this.tagsRepository.findOne({
+      id: tagid,
+    });
+    if (tags == undefined) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '권한이 없습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const now = new Date();
+    tags.name = tagname;
+    tags.updatedAt = now;
+    return await this.tagsRepository.save(tags);
   }
 
-  updateTag(id: number, updateTagDto: Tags) {
-    return `updateTag`;
-  }
-
-  removeTag(id: number) {
-    return `removeTag`;
+  async removeTags(userinfo: Users, teamid: number, tagid: number) {
+    const isjoined = await this.teamMembersRepository.findOne({
+      userid: userinfo.id,
+      teamid: teamid,
+    });
+    // no
+    if (isjoined == undefined) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '팀에 가입되어 있지 않습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    // 수정할 권한이 있는가?
+    if (isjoined.state) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error: '권한이 없습니다.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const { affected } = await this.tagsRepository.softDelete({
+      id: tagid,
+    });
+    if (affected) {
+      throw new HttpException(
+        {
+          status: HttpStatus.OK,
+          error: '테그가 삭제되었습니다.',
+        },
+        HttpStatus.OK,
+      );
+    }
+    throw new HttpException(
+      {
+        status: HttpStatus.NOT_FOUND,
+        error: '테그 삭제를 실패하였습니다.',
+      },
+      HttpStatus.NOT_FOUND,
+    );
   }
 }
