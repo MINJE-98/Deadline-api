8b548785211c89b8ff2c9de18bb248234c0ef2a4
MINJE-98 <jmj012100@gmail.com>
2021-07-23 15:01:18 +0900

    added: Items CRUD

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,62 +6,122 @@ import {
   Patch,
   Param,
   Delete,
+  Headers,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiHeader,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TokenCheck } from 'src/auth/token.check.guard';
+import { UserInfo } from 'src/common/decorator';
+import { TokenAuthError } from 'src/common/dto';
 import { Items } from 'src/entities';
+import { ItemsCreate } from './dto/itemsCreate.dto';
+import { ItemsDelete } from './dto/itemsDelete.dto';
+import { ItemsUpdate } from './dto/itemsUpdate.dto';
 import { ItemsService } from './items.service';
+@UseGuards(TokenCheck)
 @ApiTags('Items')
+@ApiHeader({
+  name: 'accesstoken',
+  required: true,
+  description: '토큰',
+})
+@ApiResponse({
+  status: 404,
+  description: '유효한 토큰이 아닙니다.',
+  type: TokenAuthError,
+})
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
-  @Post()
+  @Post(':itemname')
   @ApiOperation({
     summary: '아이템 생성',
     description: '새로운 아이템을 생성합니다.',
   })
-  @ApiResponse({ status: 200, description: '성공', type: Items })
-  createItem(@Body() createItemDto: Items) {
-    return this.itemsService.createItem(createItemDto);
-  }
-
-  @Get(':ItemName')
-  @ApiOperation({
-    summary: '아이템 특정 아이템 조회',
-    description: '아이템 특정 아이템 조회합니다.',
+  @ApiBody({
+    required: true,
+    type: ItemsCreate,
   })
   @ApiResponse({ status: 200, description: '성공', type: Items })
-  findItems(@Param('ItemName') ItemName: string) {
-    return this.itemsService.findItems(+ItemName);
+  createItems(
+    @Headers('accesstoken') AccessToken,
+    @Body() body,
+    @UserInfo('id') id: number,
+  ) {
+    return this.itemsService.createItems(body, id);
   }
 
-  @Patch(':Itemid')
+  @Get('')
+  @ApiOperation({
+    summary: '아이템 아이템 조회',
+    description: '아이템 아이템 조회합니다.',
+  })
+  @ApiQuery({
+    required: false,
+    name: 'itemname',
+  })
+  @ApiQuery({
+    required: false,
+    name: 'barcode',
+  })
+  @ApiQuery({
+    required: false,
+    name: 'itemid',
+  })
+  @ApiResponse({ status: 200, description: '성공', type: Items })
+  findItems(
+    @Headers('accesstoken') AccessToken,
+    @Query('itemname') itemname: string,
+    @Query('barcode') barcode: string,
+    @Query('itemid') itemid: number,
+  ) {
+    return this.itemsService.findItems(itemname, barcode, itemid);
+  }
+
+  @Patch(':itemid')
   @ApiOperation({
     summary: '아이템 특정 아이템 수정',
     description: '아이템 특정 아이템 수정합니다.',
   })
+  @ApiBody({
+    required: true,
+    type: ItemsUpdate,
+  })
   @ApiResponse({ status: 200, description: '성공', type: Items })
-  updateItem(@Param('Itemid') Itemid: string) {
-    return this.itemsService.updateItem(+Itemid);
+  updateItems(
+    @Headers('accesstoken') AccessToken,
+    @UserInfo('id') id: number,
+    @Body() body,
+    @Param('itemid') itemid: number,
+  ) {
+    return this.itemsService.updateItems(id, body, itemid);
   }
 
-  @Delete(':Itemid')
+  @Delete(':itemid')
   @ApiOperation({
     summary: '아이템 특정 아이템 삭제',
     description: '아이템 특정 아이템 삭제합니다.',
   })
-  @ApiResponse({ status: 200, description: '성공', type: Items })
-  removeItem(@Param('Itemid') Itemid: string) {
-    return this.itemsService.removeItem(+Itemid);
-  }
-
-  @Get('teams/:teamId')
-  @ApiOperation({
-    summary: '팀이 등록한 아이템 모두 보기',
-    description: '팀이 등록한 아이템 모두 보여줍니다.',
+  @ApiBody({
+    required: true,
+    type: ItemsDelete,
   })
   @ApiResponse({ status: 200, description: '성공', type: Items })
-  getMyTeamItem(@Param('teamId') teamId: string) {
-    return this.itemsService.findItems(+teamId);
+  removeItem(
+    @Headers('accesstoken') AccessToken,
+    @UserInfo('id') id: number,
+    @Body('teamid') teamid: number,
+    @Param('itemid') itemid: number,
+  ) {
+    return this.itemsService.removeItem(id, teamid, itemid);
   }
 }
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Items, TeamMembers, Teams, Users } from 'src/entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Teams, TeamMembers, Users, Items])],
   controllers: [ItemsController],
-  providers: [ItemsService]
+  providers: [ItemsService],
 })
 export class ItemsModule {}
